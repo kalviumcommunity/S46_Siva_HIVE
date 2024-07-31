64f61e5654ce879c83fd3734eaaac55eeecb168c
AlexCFrost <siva.ganesh.c18@gmail.com>
2024-07-31 10:53:05 +0530

    Updated schema for security

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { useAuthContext } from './Context/useAuthContext'
 
 import Home from './Slides/Home'
 import Login from './Slides/Login'
-import Signup from './Slides/Signup'
 
 function App() {
   const {user} = useAuthContext()
@@ -17,10 +16,6 @@ function App() {
         <Route
           path="/login"
           element={!user ? <Login/> : <Navigate to="/" />}
-        />
-        <Route
-          path="/signup"
-          element={!user ? <Signup/> : <Navigate to="/" />}
         />
       </Routes>
     
